54b4002dc9b450f57d655ab6b0941cbd49c7d0da
minhttt <ascf5649@gmail.com>
2025-03-29 12:40:26 +0700

    Update undo redo

--- src/main/java/MemberMemento/MemberRestore.java
+++ src/main/java/MemberMemento/MemberRestore.java
@@ -14,26 +14,46 @@ import java.util.List;
  * @author minh9
  */
 public class MemberRestore {
-    private final List <MemberMemento> backup = new ArrayList<>();
+    private final List <MemberMemento> backup = new ArrayList<>();  // 0 1 2
     private int currIndex = -1;
-   public void saveMemento(MemberMemento memberMemento) {
+   public void saveMemento(MemberMemento memberMemento) {  //  rong  
         while (backup.size() > currIndex + 1) { // for redo
             backup.remove(backup.size() - 1); //Xoa backup o gan nhat
         }
         backup.add(memberMemento);
-        currIndex++;
+        currIndex++; //0
     }
 
     // undo
     public MemberMemento undo() {
+        if(canUndo()) {
         MemberMemento memento = backup.get(currIndex);
-        currIndex--;
+        currIndex--; //-1 
         return memento;
+        } return null;
     }
 
     // redo
     public MemberMemento redo() {
+        if(canRedo()) {
             currIndex++;
             return backup.get(currIndex);
+        } return null;
     }
+    
+    public boolean canUndo()
+    {
+       boolean check =  currIndex >= 0 && !backup.isEmpty() ?  true :  false; // 
+       return check;
+    }
+    
+    
+        public boolean canRedo()
+    {
+       boolean check =  currIndex < backup.size() - 1 ?  true :  false; // 
+       return check;
+    }
+    
+    
+    
 }
